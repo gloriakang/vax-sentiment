64dca0295409f63114b6e6acc6d4d244e9902756
Gloria Kang <gkang@vt.edu>
2016-10-30 21:13:34 -0400

    update R stuff

--- output/R/plots.R
+++ output/R/plots.R
@@ -3,11 +3,12 @@
 rm(list = ls(all.names = TRUE))
 library(ggplot2)
 
+# read data
 u_Gc_nodes_neg2 = read.csv("output/df/final_undirected/u_Gc_nodes_neg2.csv")
 u_Gc_nodes_neu2 = read.csv("output/df/final_undirected/u_Gc_nodes_neu2.csv")
 u_Gc_nodes_pos2 = read.csv("output/df/final_undirected/u_Gc_nodes_pos2.csv")
 
-
+# multiplot function
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   library(grid)
 
@@ -43,44 +44,64 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   }
 }
 
+
 # positive
-a <- ggplot(u_Gc_nodes_pos2, mapping = aes(bet.cent, clo.cent, color = deg.cent, size = deg.cent)) +
-  geom_point(alpha=0.5) + geom_text(aes(label = node), alpha=0.9, hjust="inward") + ggtitle("Positive Sentiment")
-a <- a + scale_size(range = c(0,10))
+a <- ggplot(u_Gc_nodes_pos2, mapping = aes(bet.cent, clo.cent, size = deg.cent, color = deg.cent)) +
+  geom_point(alpha=0.5, position = "jitter") +
+  geom_text(aes(label = node), alpha=0.9, hjust="inward") +
+  ggtitle("Positive Sentiment")
+a <- a + scale_size(range = c(0,5))
 a
 
 # negative
 b <- ggplot(u_Gc_nodes_neg2, mapping = aes(bet.cent, clo.cent, size = deg.cent, color = deg.cent)) +
-  geom_point(alpha=0.5) + geom_text(aes(label = node), alpha = 0.9, hjust = "inward") + ggtitle("Negative Sentiment")
+  geom_point(alpha=0.5) + geom_text(aes(label = node), alpha = 0.9, hjust = "inward") + 
+  ggtitle("Negative Sentiment")
 b <- b + scale_size(range = c(0,10))
-
 b
-b + scale_x_log10() + scale_y_log10()
-
 
 # neutral
 c <- ggplot(u_Gc_nodes_neu2, mapping = aes(bet.cent, clo.cent, size = deg.cent, color = deg.cent)) +
-  geom_point(alpha=0.5) + geom_text(aes(label = node), alpha = 0.9, hjust="inward") + ggtitle("Neutral Sentiment")
+  geom_point(alpha=0.5) + geom_text(aes(label = node), alpha = 0.9, hjust="inward") + 
+  ggtitle("Neutral Sentiment")
 c <- c + scale_size(range = c(0,10))
 c
 
 multiplot(a,b,c)
 
 
+##############################
+# single plot
+
+df = read.csv("output/R/all-nodes.csv")
+
+# Greatest node centrality values with labels for positive and negative networks
+d <- ggplot(df, aes(bet.cent, clo.cent, color = sentiment, size = deg.cent)) +
+  xlim(0.05, 0.35) + ylim(0.3, NA) +
+  geom_point(position = "jitter", alpha = 0.4) +
+  geom_text(aes(label = node), size = 4.5, position = "jitter", alpha = 1, hjust = "inward") +
+  ggtitle("Top node centrality values \nfor positive and negative sentiment networks") + 
+  xlab("betweenness") + ylab("closeness") + labs(size = "degree centrality") +
+  scale_size_continuous(range = c(0,15))
+d
+
+
+# scales free facet with labels
+f <- ggplot(df, aes(clo.cent, bet.cent, color = sentiment, size = deg.cent)) +
+  geom_point(position = "jitter", alpha = 0.3) +
+  geom_text(aes(label = node), position = "jitter", alpha = 0.7, hjust = "inward") +
+  ggtitle("Node centrality measures for \nnegative, neutral, and positive sentiment networks") +
+  xlab("betweenness") + ylab("closeness") + labs(size = "degree centrality") +
+  scale_size_continuous(range = c(0,10)) +
+  facet_wrap(~sentiment, scales = "free")
+f
+
+
+
+
 
 # a = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = bet.cent, x = deg.cent)) + geom_text(aes(label = node)) + geom_jitter()
-# b = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = clo.cent, x = deg.cent)) + geom_text(aes(label = node)) + geom_jitter()
-# c = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = clo.cent, x = bet.cent)) + geom_text(aes(label = node)) + geom_jitter()
-# multiplot(a,b,c)
-
 # violin multiplot
 # a = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = clo.cent, x = bet.cent, fill = degree)) + geom_violin()
-# b = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = bet.cent, x = clo.cent, fill = deg.cent)) + geom_violin()
-# multiplot(a,b)
-
 # box plots
 # a = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = bet.cent, x = sentiment, fill = sentiment)) + geom_boxplot() + coord_flip()
-# b = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = deg.cent, x = sentiment, fill = sentiment)) + geom_boxplot() + coord_flip()
-# c = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = degree, x = sentiment, fill = sentiment)) + geom_boxplot() + coord_flip()
-# d = ggplot(data = u_Gc_nodes_pos2, mapping = aes(y = density, x = sentiment, fill = sentiment)) + geom_boxplot() + coord_flip()
-# multiplot(a,b,c,d)
